6f1b3b2ebdb354ff1c339ee429b4e6c8c957b24e
Riley Shahar <riley.shahar@gmail.com>
2022-09-28 16:47:21 -0700

    Impl a*b*c*

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(clippy::use_self)]
+
+use clap::Parser;
+use logos::Logos;
+
+
+#[derive(Logos, Debug, PartialEq)]
+enum AbcToken {
+    // Tokens can be literal strings, of any length.
+    #[regex("a*b*c*")]
+    Match,
+
+    #[error]
+    NoMatch,
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(value_parser)]
+    file: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    for line in std::fs::read_to_string(args.file)?.lines() {
+        let mut lex = AbcToken::lexer(line);
+        print!("{} ", line);
+        match lex.next() {
+            Some(AbcToken::Match) if lex.next().is_none() => print!("YES"),
+            _ => print!("NO"),
+        }
+
+        println!();
+    }
+
+    Ok(())
 }
