cd873d8a1a49b6d0fceebf9fe2df58089045fded
Sima Nerush <2002ssn@gmail.com>
2022-09-28 17:24:45 -0700

    add regex

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,16 @@ enum AbcToken {
     NoMatch,
 }
 
+#[derive(Logos, Debug, PartialEq)]
+enum ZeroOneOneToken {
+    // Tokens can be literal strings, of any length.
+    #[regex("1*(0+1)*0*")]
+    Match,
+
+    #[error]
+    NoMatch,
+}
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
@@ -22,18 +32,36 @@ struct Args {
     file: String,
 }
 
+fn matchAbc(line: &str) {
+    let mut lex = AbcToken::lexer(line);
+    print!("{} ", line);
+    match lex.next() {
+        Some(AbcToken::Match) if lex.next().is_none() => print!("YES"),
+        _ => print!("NO"),
+    }
+
+    println!();
+}
+
+fn matchZeroOneOne(line: &str) {
+    let mut lex = ZeroOneOneToken::lexer(line);
+    print!("{} ", line);
+    let next = dbg!(lex.next());
+    dbg!(lex.span());
+    match next {
+        Some(ZeroOneOneToken::Match) if lex.next().is_none() => print!("YES"),
+        _ => print!("NO"),
+    }
+
+    println!();
+}
+
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
     for line in std::fs::read_to_string(args.file)?.lines() {
-        let mut lex = AbcToken::lexer(line);
-        print!("{} ", line);
-        match lex.next() {
-            Some(AbcToken::Match) if lex.next().is_none() => print!("YES"),
-            _ => print!("NO"),
-        }
-
-        println!();
+        // matchAbc(line);
+        matchZeroOneOne(line);
     }
 
     Ok(())
